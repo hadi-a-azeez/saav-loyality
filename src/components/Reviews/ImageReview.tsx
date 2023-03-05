89265b3cc6e259a8d3ce59e3cd7f50c3af21044b
hadi-a-azeez <hadikoduvally@gmail.com>
2023-03-05 16:49:33 +0530

    fixes

--- src/components/Reviews/ImageReview.tsx
+++ src/components/Reviews/ImageReview.tsx
@@ -2,6 +2,7 @@ const ImageReview = () => {
   return (
     <div className="rounded-2xl bg-gold p-3">
       <div className="flex items-center gap-3 text-sm font-semibold text-gray-700">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/assets/icons/adidasLogo.png" alt="logo" width={40} />
         <div className="flex flex-col">
           <p>ADIDAS</p>
--- src/containers/User/store/RegisterSection.tsx
+++ src/containers/User/store/RegisterSection.tsx
@@ -1,15 +1,15 @@
-import Router from 'next/router';
+import Router from "next/router";
 
 const RegisterSection = () => {
   return (
-    <div className='flex w-full flex-col justify-center gap-4 rounded-lg bg-gray-900 px-6 py-4 md:py-12 md:px-20'>
-      <h1 className='text-center text-2xl font-semibold text-white'>
+    <div className="flex w-full flex-col justify-center gap-4 rounded-lg bg-gray-900 px-6 py-4 md:py-12 md:px-20">
+      <h1 className="text-center text-2xl font-semibold text-white">
         Be a member and get Exclusive Offers
       </h1>
       <button
-        className='rounded-lg bg-white px-4 py-3 font-bold text-gray-600'
+        className="rounded-lg bg-white px-4 py-3 font-bold text-gray-600"
         onClick={() => {
-          Router.push('/register');
+          void Router.push("/register");
         }}
       >
         Register Now
