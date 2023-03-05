a160a05707bd003dca489d89bdde546ce44ee9e8
hadi-a-azeez <hadikoduvally@gmail.com>
2023-03-05 16:58:21 +0530

    fix

--- src/server/api/routers/user.router.ts
+++ src/server/api/routers/user.router.ts
@@ -2,6 +2,6 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.prisma.users.findMany();
+    return await ctx.prisma.loyalty_users.findMany();
   }),
 });
